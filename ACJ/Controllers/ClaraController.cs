72070074d5ae2cb860f34a7e4eef1f21ac1768cf
Clara Fred <clara.elise.fred@gmail.com>
2021-04-13 19:32:50 +0200

    Update random quote model implemented

--- ACJ/Controllers/ClaraController.cs
+++ ACJ/Controllers/ClaraController.cs
@@ -1,16 +1,28 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.Http;
 using System.Threading.Tasks;
+using ACJ.Models;
 using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
 
 namespace ACJ.Controllers
 {
     public class ClaraController : Controller
     {
-        public IActionResult Index()
+        public async Task<IActionResult> Index()
         {
-            return View();
+            var quote = new QuoteModel();
+            using (var httpClient = new HttpClient())
+            {
+                using (var response = await httpClient.GetAsync("https://api.quotable.io/random"))
+                {
+                    var content = await response.Content.ReadAsStringAsync();
+                    quote = JsonConvert.DeserializeObject<QuoteModel>(content);
+                }
+            }
+            return View(quote);
         }
     }
 }
---
+++ ACJ/Models/QuoteModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ACJ.Models
+{
+    public class QuoteModel
+    {
+        public string Content { get; set; }
+        public string Author { get; set; }
+    }
+}
